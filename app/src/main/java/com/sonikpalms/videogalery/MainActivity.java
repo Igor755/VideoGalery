647ba82d1087cdd328361fb25b352069553d1907
Igor755 <metlin.igor@gmail.com>
2019-11-02 23:39:46 +0200

    Initial commit

---
+++ app/src/main/java/com/sonikpalms/videogalery/MainActivity.java
@@ -0,0 +1,34 @@
+package com.sonikpalms.videogalery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), VideoShooting.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
